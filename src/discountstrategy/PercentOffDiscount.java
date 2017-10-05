bec7a131017f5b8e2ec6e4dd294d293bfaf8059e
mjmersenski <mjmersenski@10.30.16.89>
2017-10-04 21:21:42 -0500

    initial commit to project

---
+++ src/discountstrategy/PercentOffDiscount.java
@@ -0,0 +1,11 @@
+package discountstrategy;
+
+/**
+ *
+ * @author mjmersenski
+ */
+public class PercentOffDiscount {
+    private double percentOff;
+    private double unitCost;
+    private int qty;
+}
